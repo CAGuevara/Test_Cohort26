96b7aa25ed958c04c9807565d554e1ae4151de01
Carmen Guevara <carmen.guevara75@gmail.com>
2021-11-17 15:39:28 -0500

    Complete tasks in conditionals and loops

---
+++ conditionals.js
@@ -0,0 +1,65 @@
+// ============ CONDITIONALS ============
+// Definition : Perfomr different operations based on the given condition
+// task: define whether a number is positive or negative
+// let integer = -1
+// if(integer>0){
+//     // inside curly brackets is our code block that runs IF we meet our condition
+// console.log("Positive: "+integer)
+// }else{
+//     console.log("Negative: "+integer)
+// }
+// // Task : Setting up a security website that grants users over age 18+ access to the site. write a conditional that gives users who are 18+ access and log access denied for users who do not meet the given condition.
+// let age = 19
+
+// if(age >=18){
+//     console.log("Access Granted")
+// }else{
+//     console.log("Access Denied")
+// }
+// // 
+
+// let num=0;
+// if(num >0){
+//     if(num>100){
+//         console.log("Number is Positive and greater than 100")
+//     } else if(num>100){
+//         console.log("Number is positive and less than a 100")
+//         } else {
+//             console.log("Number is equal to 100")
+//         }
+// }else if(num<0) {
+//     console.log("Number is Negative")
+// }else {
+//     console.log("Number is 0")
+// }
+
+
+// let grade = 109
+// if (grade>0 && grade<100){
+//     if(grade<55 && grade>0){
+//          console.log("The Student Failed")
+//     }else if(grade<=70){
+//         console.log("The grade is D")
+//     } else if(grade <=80){
+//         console.log("The grade is C")
+//     }else if(grade <=90){
+//         console.log("The grade is B")
+//     }else if(grade<=100){
+//         console.log("The Grade is A")
+//     }
+// }else{
+//     console.log("The grade can't be less than 0 or more than 100")
+// }
+
+
+// // for(let i=0; i<10;i++){
+// //     console.log(i)
+// // }
+
+// for(let i=1; i<10;i+=2){
+//     console.log(i)
+// }
+// for(let i=2; i<11;i=i+2){
+//     console.log(i)
+// }
+console.log(35%2 !=0)
